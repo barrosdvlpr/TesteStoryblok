9c62d284afc17e7be4f3c13a5c0e8d031bad47da
barrosdvlpr <barrosdvlpr@gmail.com>
2022-02-14 17:01:38 -0300

    index.js wonking

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,16 @@
-// import Head from "next/head";
-// import styles from "../styles/Home.module.css";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
  
-// // The Storyblok Client & hook
-// import Storyblok, { useStoryblok } from "../lib/storyblok";
-// import DynamicComponent from "../components/DynamicComponent";
+// The Storyblok Client & hook
+import Storyblok, { useStoryblok } from "../lib/storyblok";
+import DynamicComponent from "../components/DynamicComponent";
  
-// export default function Home({ story, preview }) {
-//   const enableBridge = true; // load the storyblok bridge everywhere
-//   // const enableBridge = preview; // enable bridge only in prevew mode
+export default function Home({ story, preview }) {
+  const enableBridge = true; // load the storyblok bridge everywhere
+  // const enableBridge = preview; // enable bridge only in prevew mode
  
-//   story = useStoryblok(story, enableBridge);
+  story = useStoryblok(story, enableBridge);
  
-//   return (
-//     <div className={styles.container}>
-//       <Head>
-//         <title>Create Next App</title>
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
- 
-//       <header>
-//         <h1>{story ? story.name : "My Site"}</h1>
-//       </header>
- 
-//       <DynamicComponent blok={story.content} />
-//     </div>
-//   );
-// }
- 
-// export async function getStaticProps({ preview = false }) {
-//   // home is the default slug for the homepage in Storyblok
-//   let slug = "home";
-//   // load the published content outside of the preview mode
-//   let sbParams = {
-//     version: "published", // or 'draft'
-//   };
- 
-//   if (preview) {
-//     // load the draft version inside of the preview mode
-//     sbParams.version = "draft";
-//     sbParams.cv = Date.now();
-//   }
- 
-//   let { data } = await Storyblok.get(`cdn/stories/${slug}`, sbParams);
- 
-//   return {
-//     props: {
-//       story: data ? data.story : null,
-//       preview,
-//     },
-//     revalidate: 3600, // revalidate every hour
-//   };
-// }
-
-
-import Head from "next/head"
-import styles from "../styles/Home.module.css"
- 
-// The Storyblok Client
-import Storyblok from "../lib/storyblok"
- 
-export default function Home(props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -68,24 +19,20 @@ export default function Home(props) {
       </Head>
  
       <header>
-        <h1>
-          { props.story ? props.story.name : 'My Site' }
-        </h1>
+        <h1>{story ? story.name : "My Site"}</h1>
       </header>
  
-      <main>
-        
-      </main>
+      <DynamicComponent blok={story.content} />
     </div>
-  )
+  );
 }
  
-export async function getStaticProps({ preview = false }) {
+export async function getStaticProps({ preview = true }) {
   // home is the default slug for the homepage in Storyblok
   let slug = "home";
   // load the published content outside of the preview mode
   let sbParams = {
-    version: "draft", // or 'published'
+    version: "published", // or 'draft'
   };
  
   if (preview) {
@@ -104,6 +51,59 @@ export async function getStaticProps({ preview = false }) {
     revalidate: 3600, // revalidate every hour
   };
 }
+
+
+// import Head from "next/head"
+// import styles from "../styles/Home.module.css"
+ 
+// // The Storyblok Client
+// import Storyblok from "../lib/storyblok"
+ 
+// export default function Home(props) {
+//   return (
+//     <div className={styles.container}>
+//       <Head>
+//         <title>Create Next App</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+ 
+//       <header>
+//         <h1>
+//           { props.story ? props.story.name : 'My Site' }
+//         </h1>
+//       </header>
+ 
+//       <main>
+        
+//       </main>
+//     </div>
+//   )
+// }
+ 
+// export async function getStaticProps({ preview = false }) {
+//   // home is the default slug for the homepage in Storyblok
+//   let slug = "home";
+//   // load the published content outside of the preview mode
+//   let sbParams = {
+//     version: "draft", // or 'published'
+//   };
+ 
+//   if (preview) {
+//     // load the draft version inside of the preview mode
+//     sbParams.version = "draft";
+//     sbParams.cv = Date.now();
+//   }
+ 
+//   let { data } = await Storyblok.get(`cdn/stories/${slug}`, sbParams);
+ 
+//   return {
+//     props: {
+//       story: data ? data.story : null,
+//       preview,
+//     },
+//     revalidate: 3600, // revalidate every hour
+//   };
+// }
 
 
 // export default function Home() {
